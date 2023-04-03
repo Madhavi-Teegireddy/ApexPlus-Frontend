64734da49fb130b53f677b02eccdc5c8b6cc1808
madhavi-teegireddy <madhavitigireddi@gmail.com>
2023-04-04 00:05:58 +0530

    add scenerio styling

--- react-assignment/src/Pages/AddScenerio.jsx
+++ react-assignment/src/Pages/AddScenerio.jsx
@@ -7,9 +7,48 @@ const AddScenerio = () => {
     <>
     <SideNavbar/>
 
-    <div className={style.container}>
-        Add scenerios
+    <div className={style.cont}>
+
+        <div className={style.outcontent}>
+            <h2 className={style.heading}>Add Scenerio</h2>
+            <div className={style.content}>
+
+                {/* edited form */}
+                <div className={style.container}>
+    <div className={style.content}>
+      <form action="">
+        <div className={style.details}>
+         
+          <div className={style.box}>
+            <label>Scenerio Name</label>
+            <input type="text" required/>
+          </div>
+          <div className={style.box}>
+            <label>Scenerio Time (seconds)</label>
+            <input type="text" required/>
+          </div>
+          
+        </div>
+        
+      </form>
     </div>
+  </div>
+     
+            
+
+            </div>
+        </div>
+
+        <div className={style.btn}>
+            <button style={{backgroundColor:"green", color:"white"}}>Add</button>
+            <button style={{backgroundColor:"orange", color:"white"}}>Reset</button>
+            <button style={{backgroundColor:"blue", color:"white"}}>Go Back</button>
+        </div>
+    </div>
+
+    
+
+   
     </>
   )
 }
